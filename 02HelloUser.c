c5f47992e33507c08c28e9df83b053a7aa1b9046
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-08-13 16:41:53 -0300

    added solution 03

--- 02HelloUser.c
+++ 02HelloUser.c
@@ -1,14 +1,31 @@
 /* 
-1) Write a program that displays the message 'Hello, World!' on the screen."
+02) Write a program that reads a person's name and displays a welcome message for them:
+Example:
+What's your name? João da Silva
+Hello João da Silva, it's a pleasure to meet you!"
 */
 
 #include <stdio.h>
+#include <string.h>
 
-void helloworld() {
-  printf("Hello World ! \n");
+void helloUser() {
+  char name[20];
+
+  printf("What is your name ? ");
+  fgets(name, sizeof(name), stdin);
+
+  name[strcspn(name, "\n")] = '\0';
+
+  printf("Hello %s nice to meet you ! \n", name);
+}
+
+void pressEnterToExit() {
+  printf("Press Enter to exit...");
+  getchar();
 }
 
 int main() {
-  helloworld();
+  helloUser();
+  pressEnterToExit();
   return 0;
 }
